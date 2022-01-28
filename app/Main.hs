0809b8ceba0b93b596b1f672bae86c26cafa26c6
Volodya Rachkin <vova@kpnn.ru>
2022-01-28 14:15:07 +0300

    added one way view raytracing

--- app/Main.hs
+++ app/Main.hs
@@ -5,22 +5,33 @@ import Data.Foldable
 import System.Console.ANSI
 import Lib
 
+import Graphics.UI.GLUT
+import Graphics.Rendering.OpenGL
 
 main = do
-    drawBorders
-    Just (x, y) <- getTerminalSize
-    forM_ [1..(x-2)] (\i -> do
-        setCursorPosition i 1
-        forM_ [1..(y-2)] (\j ->
-            putChar $ case (trace (i - x `div` 2) (j - y `div` 2)) of
-                0 -> ' '
-                1 -> '░'
-                2 -> '▒'
-                3 -> '▓'
-                4 -> '█'
-            )
-        )
+    getArgsAndInitialize
+    createAWindow "Points Window"
+    mainLoop
 
-    updateAll
-    threadDelay 2000000
-    --clearScreen
+createAWindow windowName = do
+    createWindow windowName
+    displayCallback $= display
+
+screenX = 100
+screenY = 100
+
+display = do
+    clear [ColorBuffer]
+    currentColor $= Color4 0 0.3 1 1
+    renderPrimitive Points(
+        do
+            forM_ [0..(screenX*2)] (\i -> do
+                setCursorPosition i 1
+                forM_ [0..(screenY*2)] (\j ->
+                    case trace (i - screenX) (j - screenY) of
+                        4 -> vertex (Vertex3 (((fromIntegral (i - screenX) / fromIntegral screenX) )::GLfloat) (fromIntegral (j - screenY) / fromIntegral screenY) 0)
+                        otherwise -> return ()
+                    )
+                )
+            )
+    flush
--- src/Lib.hs
+++ src/Lib.hs
@@ -9,6 +9,8 @@ inf = 999999
 
 newtype Coords = Coords [Double]
 
+newtype MyColor4 = MyColor4 (Double, Double, Double, Double)
+
 instance Num Coords where
     (+) (Coords x) (Coords y) = Coords $ zipWith (+) x y
     (-) (Coords x) (Coords y) = Coords $ zipWith (-) x y
@@ -32,7 +34,10 @@ class Figure f where
     distanceF :: Coords -> f -> Double
 
 
-figures = [Sphere (Coords [2, 0, 0]) 0.1] --map (\x -> Sphere (Coords [x, x, x]) 0.5) [1..1]--
+whiteColor = MyColor4 (1, 1, 1, 1)
+blackColor = MyColor4 (0, 0, 0, 1)
+
+figures = [Sphere (Coords [2, 0, 0]) 0.1, Sphere (Coords [2, 0, 2]) 0.1] --map (\x -> Sphere (Coords [x, x, x]) 0.5) [1..1]--
 
 disctance2 :: Coords -> Coords -> Double
 disctance2 (Coords x1s) (Coords x2s)  = foldr (+) 0 (zipWith (\y1 y2 -> (y1 - y2)^2) x1s x2s)
@@ -48,8 +53,8 @@ instance Figure Sphere where
 zero_coords = Coords $ take dimension $ repeat 0
 
 screen_distance = 1
-pixel_i_size = 0.028
-pixel_j_size = 0.014
+pixel_i_size = 0.01
+pixel_j_size = 0.01
 
 
 view_direction = Coords [1, 0, 0]
@@ -72,7 +77,7 @@ trace' cxs dxs = moveCoordsD cxs dxs d
     where d = min_distance cxs
 
 trace :: Int -> Int -> Int
-trace i j = if (md > 0.5) then 0 else 4
+trace i j = if (md > 0.1) then 0 else 4
     where dxs = normilize $ cxs - my_position
           cxs = my_position + view_direction `scalarMul` screen_distance + up_direction `scalarMul` (fromIntegral  i * pixel_i_size) + side_direction `scalarMul` (fromIntegral j * pixel_j_size)
           fxs = foldl (\cxs' i ->  trace' cxs' dxs) cxs [0..10]
